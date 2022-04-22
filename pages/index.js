4373890870ddd5952e998a2283d2648f8dbdeaad
williamcool7890 <62357797+williamcool7890@users.noreply.github.com>
2022-04-22 16:09:07 +0100

    2

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<div className="grid place-content-center text-center">
+			<div className="bg-[#EEE] w-screen inline-block align-middle py-4 mb-10">
+				<h1 className="text-4xl text-[#21216d] font-extrabold	">
+					How is James Superior to Phurup Mabo (2nd CatMan)
+				</h1>
+			</div>
+			<div className="">
+				<ul>
+					<li>James might be straight</li>
+				</ul>
+			</div>
+			<div className="absolute bottom-0 left-0 flex flex-row">
+				<img className=" h-[90vh]" src="/catman.png" alt="Catman" />
+				<p className="relative top-[70vh] text-4xl">
+					← This man however, is not
+				</p>
+			</div>
+		</div>
+	);
+}
